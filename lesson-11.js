cef42882579bf342f5f0f99008033e1e5c0e4907
Alex Platt <alja.platt@gmail.com>
2022-01-25 08:09:55 +0000

    added card display message

--- lesson-11.js
+++ lesson-11.js
@@ -1,5 +1,10 @@
+let messageEl = document.querySelector(".message-el");
+let sumEl = document.querySelector(".sum-el");
+let cardsEl = document.querySelector(".cards-el");
+console.log(messageEl);
+
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 2;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -8,16 +13,19 @@ let message = "";
 // 2. Reassign the message variable to the string we're logging out
 
 function startGame() {
+  sumEl.textContent = sumEl.textContent + sum;
+  cardsEl.textContent = cardsEl.textContent + `${firstCard} and ${secondCard}`;
+
   if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
+    message = "You've got Blackjack";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game";
     isAlive = false;
   }
-  console.log(message);
+  messageEl.textContent = message;
 }
 
 // 3. Log it out!
