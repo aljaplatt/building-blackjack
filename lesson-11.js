e291de89a2553908fa25180d5bac3b0dbfbbcda9
Alex Platt <alja.platt@gmail.com>
2022-01-26 08:03:03 +0000

    looping the cards arr

--- lesson-11.js
+++ lesson-11.js
@@ -5,18 +5,18 @@ console.log(messageEl);
 
 let firstCard = 10;
 let secondCard = 2;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
-// 1. Declare a variable called message and assign its value to an empty string
 let message = "";
-// 2. Reassign the message variable to the string we're logging out
 
 const startGame = () => renderGame();
 
 function renderGame() {
+  // render out cards
+  cardsEl.textContent = `Your cards are: ${cards[0]} and ${cards[1]}`;
   sumEl.textContent = "Total: " + sum;
-  cardsEl.textContent = "Cards:" + `${firstCard} and ${secondCard}`;
 
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -34,5 +34,7 @@ function newCard() {
   console.log("Drawing a new card from the deck!");
   let card = 4;
   sum += card;
+  cards.push(card);
+  console.log(cards);
   renderGame();
 }
---
+++ lesson-28.js
@@ -0,0 +1,16 @@
+// Count to ten!
+
+// We need to specify...
+
+// Where should we START counting?
+// Where is the FINISH line?
+// What's the STEP SIZE we should use?
+
+//    START - 1    FINISH @ 10  STEP SIZE + 1
+for (let count = 1; count < 11; count += 1) {
+  console.log(count);
+}
+
+for (let index = 10; index < 101; index += 10) {
+  console.log(index);
+}
