59c04c4bbc953746e06e1c843fb85b608b798c6d
Alex Platt <alja.platt@gmail.com>
2022-01-26 08:27:02 +0000

    looping the cards arr

--- lesson-28.js
+++ lesson-28.js
@@ -8,9 +8,29 @@
 
 //    START - 1    FINISH @ 10  STEP SIZE + 1
 for (let count = 1; count < 11; count += 1) {
-  console.log(count);
+  //   console.log(count);
 }
 
 for (let index = 10; index < 101; index += 10) {
-  console.log(index);
+  //   console.log(index);
+}
+
+let messages = [
+  "Hey, how's it going?",
+  "I'm great, thank you! How about you?",
+  "All good. Been working on my portfolio lately.",
+  "Same here!",
+];
+
+for (i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
+
+let cards = [7, 3, 9];
+
+// Create a for loop that logs out all the cards in the array
+// Use cards.length to specify how long the loop should run
+
+for (i = 0; i < cards.length; i++) {
+  console.log(cards[i]);
 }
